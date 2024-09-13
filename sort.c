5faa7fa0fe1d078464b8f708934f8d6801034ab1
Mark Kochanov <gmrak1990@gmail.com>
2024-09-14 01:14:20 +0300

    fix

--- sort.c
+++ sort.c
@@ -1,21 +1,25 @@
 #include "sort.h"
 
-void bubble_sort(int *data, unsigned int n) {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (i < j && data[i] > data[j]) {
-                int tmp = data[i];
-                data[i] = data[j];
-                data[j] = tmp;
+void swap(int *a, unsigned int i, unsigned int j) {
+    int tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+}
+
+void bubble_sort(int *data, int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - 1; ++j) {
+            if (data[j] > data[j + 1]) {
+                swap(data, j, j + 1);
             }
         }
     }
 }
 
-void insertion_sort(int *data, unsigned int n) {
+void insertion_sort(int *data, int n) {
 
 }
 
-void selection_sort(int *data, unsigned int n) {
+void selection_sort(int *data, int n) {
 
 }
--- sort.h
+++ sort.h
@@ -1,8 +1,8 @@
 #ifndef SORTS_H
 #define SORTS_H
 
-void bubble_sort(int *data, unsigned int n);
-void insertion_sort(int *data, unsigned int n);
-void selection_sort(int *data, unsigned int n);
+void bubble_sort(int *data, int n);
+void insertion_sort(int *data, int n);
+void selection_sort(int *data, int n);
 
 #endif //SORTS_H
