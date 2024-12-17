f027f7aabebdefbe268fb6455d3b1a93cc9aca6d
Mzyhh <xiiad@mail.ru>
2024-12-17 21:48:39 +0300

    rebase

--- src/main.c
+++ src/main.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "sort.h"
-#include "nth_element.h"
-#include "bignum.h"
-
-void printArray(const int* const array, const int n) {
-    for (int i = 0; i < n; ++i)
-        printf("array[%d] = %d\n", i, array[i]);
-}
+#include "tree_23.h"
 
 int main(void) {
-    int a[] = {0};
-    int b[] = {0};
-    bignum n1 = {a, 1},
-           n2 = {b, 1};
-    bignum result = karatsuba_binary_numbers(n1, n2);
-    printf("a:\n");
-    printArray(a, 1);
-    printf("b:\n");
-    printArray(b, 1);
-    printf("c:\n");
-    printArray(result.digits, 2);
+    node_23 *root = NULL;
+    add(root, 1);
     return 0;
 }
