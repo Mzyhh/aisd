f745b762a28312d8fe64e6abb201a55708f7db0e
Mzyhh <xiiad@mail.ru>
2025-04-01 21:20:38 +0300

    Implement persistent l1-list

--- src/main.c
+++ src/main.c
@@ -1,51 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "list_persistent.h"
 
-#include "tree_23.h"
-
-void print_node(node_23 *node) {
-    if (node->type == node_2) {
-        printf("node_2:\nvalue_left = %d\n", node->value_left);
-        if (node->left)
-            printf("left = %d\nright = %d\n", node->left->value_left, node->right->value_left);
-        if (node->parent)
-            printf("parent = %d\n", node->parent->value_left);
-    } else {
-        printf("node_3:\n");
-        printf("value_left = %d\nvalue_right = %d\n", node->value_left, node->value_right);
-        if (node->left)
-            printf("left = %d\nmiddle = %d\nright = %d\n", node->left->value_left, node->middle->value_left, node->right->value_left);
-        if (node->parent)
-            printf("parent = %d\n", node->parent->value_left);
-    }
-    printf("\n");
-}
-
-void print_all_nodes(node_23 *tree) {
-    if (!tree) return;
-    print_node(tree);
-    print_all_nodes(tree->left);
-    if (tree->type == node_3)
-        print_all_nodes(tree->middle);
-    print_all_nodes(tree->right);
+void print_node(int value) {
+    fprintf(stderr, "%d ", value);
 }
 
 int main(void) {
-    srand(time(NULL));
-    node_23 *root = NULL;
-    int n;
-    scanf("%d", &n);
-    int *number = (int*)malloc(n*sizeof(int));
-    for (int i = 0; i < n; ++i) {
-        number[i] = rand() % 1000;
-        root = add(root, number[i]);
+    list_persistent list = create_empty();
+    insert_at(&list, 0, 1);
+    insert_at(&list, 1, 2);
+    insert_at(&list, 2, 3);
+    insert_at(&list, 0, 4);
+    insert_at(&list, 0, 5);
+    insert_at(&list, 5, 6);
+    set(&list, 4, 100);
+    set(&list, 4, 200);
+    set(&list, 4, 300);
+    for (int i = 0; i <= list.last_version; ++i) {
+        fprintf(stderr, "version %d: ", i);
+        iterate(&list, i, print_node);
+        fprintf(stderr, "\n");
     }
-    char *res = malloc(100000*sizeof(char));
-    print(root, res);
-    printf("\n%s\n\n", res);
-    print_all_nodes(root);
-    int_pair p = get_shortest_longest_path(root);
-    printf("%d %d", p.n1, p.n2);
     return 0;
 }
---
+++ tests/list_persistent.cxx
@@ -0,0 +1,8 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_vector.hpp>
+#include "catch2/benchmark/catch_benchmark.hpp"
+
+extern "C" {
+#include "list_persistent.h"
+}
+
